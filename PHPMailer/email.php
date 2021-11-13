d803156fbd16d86870898fad6b4116c3735758b2
Tomás Agüero Pizzi <tomas.ap@live.com>
2021-11-12 21:06:06 -0300

    PHPMailer, Creación cuentas como Admin
    Se configuro el PHPMailer con un GMail que se creó para uso con este TP.
    
    Se corrigieron errores menores en creación de cuentas como Admin.

--- PHPMailer/email.php
+++ PHPMailer/email.php
@@ -53,6 +53,46 @@
                 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
         }
+
+        public function send($email)
+        {
+            
+
+            // Instantiation and passing `true` enables exceptions
+            $mail = new PHPMailer(true);
+            
+            try {
+                //Server settings
+                //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                $mail->isSMTP();                                            // Send using SMTP
+                $mail->Host       = 'ssl://smtp.gmail.com:465';     //borrar ssl:// y :465               // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = EMAIL;               // "youremail@gmail.com"      // SMTP username
+                $mail->Password   = EMAIL_PASS;            // 'yourpassword'                   // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    // "ssl"     // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above//25//587
+            
+                //Recipients
+                $mail->setFrom(EMAIL, 'Admin');     // $email, 'Admin'
+                $mail->addAddress($email);    // "youremail@gmail.com" // Add a recipient//usuario
+            
+            
+                // Attachments
+                //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                //$mail->addAttachment($qr->qrCode(300), 'new.jpg');    // Optional name
+            
+                // Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'Nueva Empresa';
+            
+                $mail->Body= "Se le comunica que una nueva empresa se ha creado";
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
     }
 
 
