b14e5d1b3d7b0a3d5c9eeb3523c2e719b8059d80
Federico <fedu_zero@hotmail.com>
2021-11-15 21:49:48 -0300

    Seguir en sesión luego de cerrar la pagina.

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,10 +2,34 @@
 
 namespace Controllers;
 
+use controllers\AccountController as AccountController;
+
 class HomeController {
+
     public function Index(){
-        require_once(VIEWS_PATH."header.php");
-        require_once(VIEWS_PATH."login.php");
+
+        if (isset($_SESSION['account'])){
+
+            $accountController = new AccountController();
+
+            if($_SESSION['account']->getPrivilegios() == "admin"){
+
+                $accountController->showList();
+
+            }
+            else if($_SESSION['account']->getPrivilegios() == "student"){
+
+                $accountController->viewAccount();
+
+            }
+            else if($_SESSION['account']->getPrivilegios() == "company"){
+                $accountController->viewCompany();
+            }
+        }
+        else{
+            require_once(VIEWS_PATH."header.php");
+            require_once(VIEWS_PATH."login.php");
+        }
     }
 }
 ?>
